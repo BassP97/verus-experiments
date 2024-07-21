828f203775982de321797ea0f3be8fc89e423cea
Paul Bass <pbass@utexas.edu>
2024-07-20 23:24:18 -0500

    naming

--- twoSum/src/main.rs
+++ twoSum/src/main.rs
@@ -12,44 +12,46 @@ verus! {
         use vstd::prelude::*;
 
         // Inner recursive function to check all pairs
-        spec fn check_pairs_recursive(nums: Seq<u32>, target: u32, fixed_index: nat, floating_index: nat) -> bool
+        spec fn check_pairs_including_num_at_index(nums: Seq<u32>, target: u32, fixed_index: nat, floating_index: nat) -> bool
         decreases floating_index
         {
             // Base case: If all pairs are checked
             // case 2, if floating index val plus fixed index val == target, we return
             // case 3, recurse using the next possible floating index val
-            return floating_index > 0 && ((floating_index != fixed_index && (nums[fixed_index as int] + nums[floating_index as int] == target)) || check_pairs_recursive(nums, target, fixed_index, (floating_index - 1) as nat));
+            floating_index > 0 &&
+                (
+                    (floating_index != fixed_index && (nums[fixed_index as int] + nums[floating_index as int] == target))
+                    ||
+                    check_pairs_including_num_at_index(nums, target, fixed_index, (floating_index - 1) as nat)
+                )
         }
 
-        spec fn contains_two_elements_before_or_at_index1_that_add_up(nums: Seq<u32>, target: u32, index1: nat) -> (res: bool)
+        spec fn contains_two_elements_that_add_to_target(nums: Seq<u32>, target: u32, index1: nat) -> (res: bool)
         decreases index1,
         {
-            if index1 <= -1 || nums.len() <= 1 {
-                return false;
-            }
-            // iterate over all the possible combinations that involve the integer at this index
-            if(check_pairs_recursive(nums, target, index1, (nums.len()-1) as nat)){
-                return true;
-            }
-            // Move to the next fixed index
-            contains_two_elements_before_or_at_index1_that_add_up(nums, target, (index1-1) as nat)
+            (index1 > -1 && nums.len() > 1)
+            && (
+                (check_pairs_including_num_at_index(nums, target, index1, (nums.len()-1) as nat))
+                ||
+                contains_two_elements_that_add_to_target(nums, target, (index1-1) as nat)
+            )
         }
 
         fn two_sum(s: Vec<u32>, target:u32) -> (res: bool)
         requires
-            forall|i: nat| i < s.len() ==> s[i as int] < 100000000 && s[i as int] > 0 , // overflow? better way of doing this?
+            forall|i: nat| i < s.len() ==> s[i as int] < 100000000 && s[i as int] >= 0 , // overflow? better way of doing this?
             0 < target < 100000000,
         ensures
-            res == contains_two_elements_before_or_at_index1_that_add_up(s@, target, s@.len())
+            res == contains_two_elements_that_add_to_target(s@, target, s@.len())
         {
             let mut seen:Vec<u32> = Vec::new();
             let mut found_sum = false;
             let mut idx = 0;
 
-            while idx <  s.len()
+            while idx < s.len()
                 invariant
                     idx <= s.len(),
-                    exists|x: int, y: int| x < idx && y < idx && s[x] + s[y] == target <==> found_sum,
+                    exists|x: int, y: int| (x < idx && y < idx && && x != y && s[x] + s[y] == target) <==> found_sum,
             {
                 let elem = s[idx];
                 let lookingFor = target - elem;
