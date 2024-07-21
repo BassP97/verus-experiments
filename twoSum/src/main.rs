73132596a1d9b5c874b17ed38eb6473866455b59
Paul Bass <pbass@utexas.edu>
2024-07-20 23:01:40 -0500

    first shot at a spec driven version

--- twoSum/src/main.rs
+++ twoSum/src/main.rs
@@ -1,81 +1,82 @@
 use vstd::prelude::*;
 
 /*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Given an array of integers nums and an integer target, return true if there exist two ints at different indices that add up to the target
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 You can return the answer in any order.
 */
 verus! {
-
     mod two_sum_module {
         use vstd::prelude::*;
-        use std::collections::HashSet;
 
-        #[verifier::reject_recursive_types(T)]
-        #[verifier::external_body]
-        struct ExternalHashSet<T> {
-            inner: HashSet<T>,
-        }
-
-        #[verifier::external_body]
-        impl<T> ExternalHashSet<T> 
-        where
-            T: std::cmp::Eq + std::hash::Hash + builtin::Integer,
+        // Inner recursive function to check all pairs
+        spec fn check_pairs_recursive(nums: Seq<u32>, target: u32, fixed_index: nat, floating_index: nat) -> bool
+        decreases floating_index
         {
-            #[verifier::external_body]
-            pub fn new() -> Self {
-                ExternalHashSet {
-                    inner: HashSet::new(),
-                }
-            }
-
-            #[verifier::external_body]
-            pub fn insert(&mut self, value: T) -> (res: Set<int>)
-            ensures
-                res == set![value as int]
-            {
-                self.inner.insert(value);
-                return set![value as int];
-            }
-        
-            #[verifier::external_body]
-            pub fn contains(&self, value: &T) -> (res: bool)
-
-            {
-                self.inner.contains(value)
-            }
+            // Base case: If all pairs are checked
+            // case 2, if floating index val plus fixed index val == target, we return
+            // case 3, recurse using the next possible floating index val
+            return floating_index > 0 && ((floating_index != fixed_index && (nums[fixed_index as int] + nums[floating_index as int] == target)) || check_pairs_recursive(nums, target, fixed_index, (floating_index - 1) as nat));
         }
 
-        fn two_sum(s: Vec<u64>, target:u64) -> (res: bool)
+        spec fn contains_two_elements_before_or_at_index1_that_add_up(nums: Seq<u32>, target: u32, index1: nat) -> (res: bool)
+        decreases index1,
+        {
+            if index1 <= -1 || nums.len() <= 1 {
+                return false;
+            }
+            // iterate over all the possible combinations that involve the integer at this index
+            if(check_pairs_recursive(nums, target, index1, (nums.len()-1) as nat)){
+                return true;
+            }
+            // Move to the next fixed index
+            contains_two_elements_before_or_at_index1_that_add_up(nums, target, (index1-1) as nat)
+        }
+
+        fn two_sum(s: Vec<u32>, target:u32) -> (res: bool)
         requires
             forall|i: nat| i < s.len() ==> s[i as int] < 100000000 && s[i as int] > 0 , // overflow? better way of doing this?
             0 < target < 100000000,
         ensures
-            exists|x: nat, y: nat| x < s.len() && y < s.len() && s[x as int] + s[y as int] == target <==> res
+            res == contains_two_elements_before_or_at_index1_that_add_up(s@, target, s@.len())
         {
-            let mut seen = ExternalHashSet::new(); 
+            let mut seen:Vec<u32> = Vec::new();
             let mut found_sum = false;
             let mut idx = 0;
 
             while idx <  s.len()
                 invariant
                     idx <= s.len(),
-                    exists|x: int, y: int| x < idx && y < idx && s[x] + s[y] == target <==> found_sum
+                    exists|x: int, y: int| x < idx && y < idx && s[x] + s[y] == target <==> found_sum,
             {
                 let elem = s[idx];
-                let lookingFor: u64 = target - elem;
-                if (seen.contains(&lookingFor)){
-                    found_sum = true;
+                let lookingFor = target - elem;
+                let idx2 = 0;
+                while idx2 < seen.len() {
+                    if seen[idx2] == lookingFor {
+                        found_sum = true;
+                    }
                 }
                 idx += 1;
             }
             found_sum
         }
+
+        fn prove_two_sum(){
+            let mut v: Vec<u32> = Vec::new();
+            v.push(0);
+            v.push(10);
+            v.push(20);
+            v.push(30);
+            v.push(40);
+            let val = two_sum(v, 70);
+        }
+
     }
 
     fn main() {
-        
+
     }
 } // verus!
