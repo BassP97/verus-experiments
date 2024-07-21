5c886cba0a3a76d0f10967eb7d4eaabdac582864
Paul Bass <pbass@utexas.edu>
2024-07-20 20:03:07 -0500

    progress so far

--- twoSum/src/main.rs
+++ twoSum/src/main.rs
@@ -8,41 +8,62 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 */
 verus! {
-    mod M1 {
-        use builtin::*;
 
-        pub closed spec fn min(x: i32, y: i32) -> i32 {
-            if x <= y {
-                x
-            } else {
-                y
-            }
+    mod two_sum_module {
+        use vstd::prelude::*;
+        use std::collections::BTreeSet;
+
+        #[verifier::external_fn_specification]
+        fn addElement(set: &mut BTreeSet<u32>, element: u32) -> (res: Set<int>)
+        ensures
+            res == set![element as int]
+        {
+            set.insert(element);
+            set![element as int]
         }
 
-        pub proof fn lemma_min(x: i32, y: i32)
-            ensures
-                min(x,y) <= x && min(x,y) <= y,
-                x < y ==> min(x, y) == x,
-                x > y ==> min(x, y) == y,
-                x == y ==> min(x, y) == x,
-        {}
-    }
+        #[verifier::external_fn_specification]
+        fn containsElement(set: &mut BTreeSet<u32>, element: u32, ghostSet: Set<int>) -> (res: bool)
+        ensures
+            res <==> ghostSet.contains(element as int)
+        {
+            #[verifier::external_fn_specification]
+            set.contains(&element)
+        }
 
-    mod M2 {
-        use builtin::*;
-        use crate::M1::*;
+        fn two_sum(s: Vec<u32>, target:u32) -> (res: bool)
+        requires
+            forall|i: int| 0 <= i < s.len() ==> 0 < s[i] < 100000000, // overflow? better way of doing this?
+            0 < target < 100000000,
+        ensures
+            exists|x: int, y: int| s[x] + s[y] == target <==> res
+        {
+            let mut seen: BTreeSet<u32> = BTreeSet::new(); 
+            let mut found_sum = false;
+            let n = s.len();
+            let mut idx = 0;
 
-        fn test() {
-            let x: i32 = 100;
-            let y: i32 = x-1;
-            proof {
-                lemma_min(x,y);
+            let seenGhostSet: Set<int> =  Set::empty();
+            while idx < n
+                invariant
+                    idx <= n,
+                    exists|x: int| seenGhostSet.contains(x) && x + s[idx as int] == target <==> found_sum
+            {
+                let elem = s[idx];
+                let lookingFor = target - elem;
+                if (seen.contains(&lookingFor)){
+                    found_sum = true;
+                }
+                let singletonGhostElemSet: Set<int> = addElement(&mut seen, elem);
+                proof {
+                    seenGhostSet = seenGhostSet + singletonGhostElemSet;
+                    assert(seenGhostSet.contains(elem as int));
+                }
+                idx -= 1;
             }
-            assert(min(x, y) ==x); // succeeds
+            found_sum
         }
     }
-
-
 
 fn main() {
 
